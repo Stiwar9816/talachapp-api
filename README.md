8fe6e8bf493b24c122c47c4562cde6fc69da46e2
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-04-21 01:55:58 -0500

    Configuración inicial

---
+++ README.md
@@ -0,0 +1,41 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# TalachAPP API
+
+1. Clonar el repositorio
+
+2. Instalar las depencias con el comando:
+
+```
+yarn install o npm install
+```
+
+3. Clonar el archivo `.env.template` y renombrarlo a `.env`
+
+4. Cambiar las variables de entorno
+
+5. Levantar la base de datos postgres
+
+```
+docker-compose up -d o docker-compose up (Para revisar los logs)
+```
+
+6. Levantar el proyecto en modo de desarrollo con el comando:
+
+```
+yarn start:dev
+```
+
+7. Documentación API en Swagger UI
+
+```
+http://localhost:3000/api
+```
+
+## Stack Usado
+
+- NestJS
+- Docker
+- Postgres
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const logger = new Logger('Bootstrap')
+  const app = await NestFactory.create(AppModule);
+  // app.enableCors(); // Configuración opcional
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+
+  // Config Swagger
+  const config = new DocumentBuilder()
+    .setTitle('TalachAPP API')
+    .setDescription('TalachApp endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('API', app, document);
+
+  await app.listen(process.env.PORT);
+  logger.log(`App runnig on port ${process.env.PORT}`)
+}
+bootstrap();
