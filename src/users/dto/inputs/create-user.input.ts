5385a104d6398ec89d3fa00afecc4cf39e22cad7
DevDonal <darenteria@miuniclaretiana.edu.co>
2023-04-30 14:56:17 -0500

    Endpoints Finalizados

--- src/users/dto/inputs/create-user.input.ts
+++ src/users/dto/inputs/create-user.input.ts
@@ -1,7 +1,32 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsString, IsNotEmpty, IsNumber, IsPositive, IsIn, IsInt, IsBoolean } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+
+  @IsString()
+  @Field(() => String)
+  name: string
+
+  @IsInt()
+  @IsPositive()
+  @Field(() => Int)
+  phone: number
+
+  @IsString()
+  @Field(() => String)
+  email: string
+
+  @IsBoolean()
+  @Field(() => Boolean)
+  isActive: boolean
+
+  @IsString()
+  @Field(() => String)
+  password: string
+  
+  @IsString()
+  @IsIn(['usuario', 'talachero'])
+  @Field(() => [String])
+  state: string[]
 }
