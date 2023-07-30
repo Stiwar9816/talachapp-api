40db32ea8d230ac37bf6d864979f150933e6ad07
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-07-29 22:06:00 -0500

    concat randomUUID name image

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,23 +1,16 @@
-import { Controller, Get, Param, Redirect, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { ImagesService } from './images.service';
-import { join } from 'path';
 import { Response } from 'express';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
-  // @Get(':filename')
-  // serveImage(@Param('filename') filename: string, @Res() res: Response) {
-  //   // Utiliza express.static para servir las imágenes
-  //   return res.sendFile(filename, { root: join(process.cwd(), 'uploads') });
-  // }
   @Get(':filename')
   async serveImage(@Param('filename') filename: string, @Res() res: Response) {
     try {
       // Obtener la URL de la imagen desde el servicio
       const imageUrl = await this.imagesService.getImageUrl(filename);
-
       // Redirigir al usuario a la URL de la imagen
       return res.redirect(imageUrl);
     } catch (error) {
--- src/prices/prices.service.ts
+++ src/prices/prices.service.ts
@@ -1,7 +1,5 @@
 import {
   BadRequestException,
-  HttpException,
-  HttpStatus,
   Injectable,
   InternalServerErrorException,
   Logger,
@@ -17,14 +15,10 @@ import { User } from 'src/users/entities/user.entity';
 import { UserRoles } from 'src/auth/enums/user-role.enum';
 import { CompaniesIdArgs } from 'src/orders/dto';
 import { CompaniesService } from 'src/companies/companies.service';
-import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
-import { createWriteStream } from 'fs';
-import { join } from 'path';
-import { query } from 'express';
-import { async } from 'rxjs';
-import { FileUpload } from './interfaces/fileupload.interface';
-import { Readable } from 'stream';
+// FileUpload
+import { v4 as uuidv4 } from 'uuid';
 import { s3Client } from './utils/s3Client';
+import { FileUpload } from './interfaces/fileupload.interface';
 
 @Injectable()
 export class PricesService {
@@ -44,7 +38,6 @@ export class PricesService {
   ): Promise<Price> {
     const { idCompany } = company;
     const { name } = createPriceInput;
-
     try {
       // Verificar si existe un precio con el mismo nombre y el mismo usuario
       const sameUserPrice = await this.priceRepository
@@ -68,38 +61,26 @@ export class PricesService {
 
       if (file) {
         const { filename, createReadStream } = await file;
-        newPrice.image = filename;
+        const uuid = uuidv4();
+        const nameImage = `${uuid}-${filename}`;
+        newPrice.image = nameImage;
 
-        // Obtén el stream del archivo FileUpload
-        const fileStream = createReadStream();
-        // Crea un objeto de parámetros para la operación PutObjectCommand
-        const putParams = {
-          Bucket: process.env.BUCKET_NAME, // Reemplaza con el nombre de tu espacio de DigitalOcean
-          Key: filename,
-          Body: fileStream as Readable,
+        const uploadParams = {
+          Bucket: process.env.BUCKET_NAME,
+          Key: nameImage,
+          Body: createReadStream(),
         };
 
         try {
-          await s3Client.upload(putParams).promise();
+          await s3Client.upload(uploadParams).promise();
         } catch (error) {
           this.handleDBException(error);
         }
-        // Envía el archivo a DigitalOcean Spaces usando la operación PutObjectCommand
-        // // Guardar la imagen en el servidor
-        // createReadStream()
-        //   .pipe(createWriteStream(join(process.cwd(), `./uploads/${filename}`)))
-        //   .on('error', () => {
-        //     throw new Error('Could not save image');
-        //   });
       }
 
       return await this.priceRepository.save(newPrice);
     } catch (error) {
-      // Handle the error properly
-      throw new HttpException(
-        error.message || 'Could not create price',
-        HttpStatus.BAD_REQUEST,
-      );
+      this.handleDBException(error);
     }
   }
 
